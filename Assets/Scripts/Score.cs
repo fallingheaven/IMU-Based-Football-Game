db99625d0d9c9d801330aab72bac3af7f95432ea
fallingheaven <296142289@qq.com>
2023-11-05 23:41:49 +0800

    做个计分的UI模型
    初学UI Toolkit，试试水

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Score : MonoBehaviour
 {
     private static Score _instance;
     private static long _score;
-
+    private VisualElement _scoreBoard;
+    private Label _scoreInfo;
+        
     public static Score Instance
     {
         get
@@ -20,18 +23,20 @@ public class Score : MonoBehaviour
             return _instance;
         }
     }
-
-    private void FixedUpdate()
-    {
-        Debug.Log(_score);
-    }
-
-    // Start is called before the first frame update
+    
     private void Start()
     {
         _score = 0;
+        _scoreBoard = GetComponent<UIDocument>().rootVisualElement;
+        _scoreInfo = _scoreBoard.Q<Label>("ScoreLabel");
     }
-
+    
+    private void FixedUpdate()
+    {
+        Debug.Log(_score);
+        _scoreInfo.text = $"分数：{_score}";
+    }
+    
     public static void UpdateScore(int a)
     {
         _score += a;
